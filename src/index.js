b47743f8341de7c2f22d30a937e872436ec92f06
Dzmitry Stolbou <persik1223@gmail.com>
2020-03-21 16:32:08 +0300

    feat: add culculate func

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let count_brackets = 0;
+    for (let i = 0; i < expr.length; i++){
+        if (expr[i] == '(') count_brackets++;
+        if (expr[i] == ')') count_brackets--;
+    }
+    if (count_brackets != 0 ) throw "ExpressionError: Brackets must be paired";
+    if (expr.indexOf('/ 0') != -1 || expr.indexOf('/0') != -1) throw "TypeError: Division by zero.";
+    expr = expr.replace(/ /g, "") && new Function(`return ${expr}`);
+    return expr();
 }
 
 module.exports = {
